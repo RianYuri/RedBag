aa7919638b5c43113fd6a9ea772dfd6e77b55581
3º DSM <dsm-3@fatec.academico>
2023-03-30 22:35:32 -0300

    feat: response adjust

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-let container = document.getElementById("main")
+const container = document.getElementById("main");
+const emailSenhaInputs = document.querySelectorAll('.input-email, .input-senha');
+const rectangleTop = document.querySelector('.first-rectangle');
 
-setTimeout(() => {
-    container.style.display="none"
-}, 4000);
+setTimeout(() => container.style.display = "none", 4000);
+
+emailSenhaInputs.forEach(input => {
+  input.addEventListener('focus', () => rectangleTop.classList.add('move-up'));
+  input.addEventListener('blur', () => rectangleTop.classList.remove('move-up'));
+});
