859df68e4760c6344b88b03b0237c50986b798d2
3º DSM <dsm-3@fatec.academico>
2023-04-06 20:57:06 -0300

    feat: animation dont account

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const container = document.getElementById("main");
 const emailSenhaInputs = document.querySelectorAll('.input-email, .input-senha');
 const rectangleTop = document.querySelector('.first-rectangle');
+const animationAccount = document.getElementById("dontAccount");
+const rectangleTopImg = document.getElementById("rectangle-top");
+const rectangleBotImg = document.getElementById("rectangle-bot");
 
 setTimeout(() => container.style.display = "none", 4000);
 
@@ -8,3 +11,8 @@ emailSenhaInputs.forEach(input => {
   input.addEventListener('focus', () => rectangleTop.classList.toggle('move-up'));
   input.addEventListener('blur', () => rectangleTop.classList.toggle('move-up'));
 });
+animationAccount.addEventListener("click",()=>{
+rectangleTopImg.classList.toggle("animation-step__top")
+rectangleBotImg.classList.toggle("animation-step__bot")
+}
+)
