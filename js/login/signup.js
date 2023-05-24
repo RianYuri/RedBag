68f37e4db9de15011248dda57df483d3c9ae1d9d
RianYuri <rianyurirgt@gmail.com>
2023-05-24 19:16:57 -0300

    feat: add api signup

--- js/login/signup.js
+++ js/login/signup.js
@@ -1,39 +1,25 @@
-const inputName = document.querySelector(".input-name");
-const inputEmail = document.querySelector(".input-email");
-const inputPassword = document.querySelector(".input-senha");
+const formSignup = document.getElementById("signup-form");
+formSignup.addEventListener("submit", () => {
+  let inputName = document.querySelector(".input-name").value;
+  let inputEmail = document.querySelector(".input-email").value;
+  let inputPassword = document.querySelector(".input-senha").value;
+  // Dados do objeto JSON a ser enviado
+  
+  let data = {
+    name: inputName,
+    email: inputEmail,
+    password: inputPassword,
+  };
 
-// Dados do objeto JSON a ser enviado
-let nameInput = '';
-let emailInput = '';
-let passwordInput = '';
-
-    inputName.addEventListener("change", (e) => {
-      nameInput = e.target.value;
-    });
-    inputEmail.addEventListener("change", (e) => {
-      emailInput = e.target.value;
-    });
-    inputPassword.addEventListener("change", (e) => {
-      passwordInput = e.target.value;
-    });
-    const data = {
-      name: nameInput,
-      email: emailInput,
-      password: passwordInput,
-    };
-    
-
-// Enviar a requisição
-const url = "http://127.0.0.1:5502/signup";  
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-};
-const requestSignup = () => {
-    console.log(data)
+  const url = "http://127.0.0.1:5501/signup";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  console.log(data);
 
   // Configurar a requisição
   fetch(url, options)
@@ -44,4 +30,4 @@ const requestSignup = () => {
     .catch((error) => {
       console.log("Ocorreu um erro:", error);
     });
-};
+});
