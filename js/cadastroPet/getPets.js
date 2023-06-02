0e9b4899840ee7c9fe74141cb1ef22afbc171109
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-06-02 20:33:58 -0300

    feat: getting pets on pets list

--- js/cadastroPet/getPets.js
+++ js/cadastroPet/getPets.js
@@ -1,8 +1,9 @@
 const getPetsInfo = async () => {
+    let itemCard = document.querySelector('.item-card');
     let userId = JSON.parse(localStorage.getItem('userId'));
     const response = await fetch(`http://127.0.0.1:5502/findanimals/${userId}`);
-    const data = await response.json();
-    console.log(data)
+    const data = (await response.json()).animals;
+    
 }
 
 document.addEventListener('DOMContentLoaded', getPetsInfo);
