05ccea562625fa3136d5fa37489bdc98cc9afc0f
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-06-03 23:49:16 -0300

    feat: create, update and delete pet

--- js/cadastroPet/getPets.js
+++ js/cadastroPet/getPets.js
@@ -1,14 +1,32 @@
 const getPetsInfo = async () => {
-    let itemsList = document.querySelector('.animals__registered-list');
-    let itemCard = document.querySelector('.animals__registered-list .animals__registered-list-item.notloaded');
-    let userId = JSON.parse(localStorage.getItem('userId'));
+    const itemsList = document.querySelector('.animals__registered-list');
+    const itemCard = document.querySelector('.animals__registered-list .animals__registered-list-item.notloaded');
+    const userId = JSON.parse(localStorage.getItem('userId'));
     const response = await fetch(`http://127.0.0.1:5502/findanimals/${userId}`);
-    const data = (await response.json()).animals;
+    const data = (await response.json()).animals[0];
+    itemCard.classList.add('hidden');
+  
     data.forEach(item => {
-        let tempCardToAdd = itemCard.cloneNode(true);
-        // tempCardToAdd.classList.remove('notloaded');
-        itemsList.appendChild(tempCardToAdd);
+      const tempCardToAdd = itemCard.cloneNode(true);
+      tempCardToAdd.classList.remove('notloaded');
+      tempCardToAdd.classList.remove('hidden');
+      tempCardToAdd.querySelector('.name-img p').innerHTML = item.name;
+      tempCardToAdd.querySelector('.name-img p').style.color = item.color;
+      const catIcon = tempCardToAdd.querySelector('.name-img svg.cat__item-icon path');
+      const newColor = item.color;
+      catIcon.setAttribute('fill', newColor);
+      catIcon.setAttribute('stroke', newColor);
+    
+      const analysisCount = item.prediagnosis?.length > 0 ? item.prediagnosis.length : 0;
+      tempCardToAdd.querySelector('.numeros-analisados').innerHTML = `${analysisCount} Análises realizadas`;
+  
+      tempCardToAdd.querySelector('.cat__img-example').src = `data:image/png;base64,${item.image}`;
+  
+      tempCardToAdd.dataset.id = item.animalID;
+      tempCardToAdd.addEventListener('click', () => editPetPage(item))
+      itemsList.appendChild(tempCardToAdd);
     });
-}
-
-document.addEventListener('DOMContentLoaded', getPetsInfo);
+  };
+  
+  document.addEventListener('DOMContentLoaded', getPetsInfo);
+  
