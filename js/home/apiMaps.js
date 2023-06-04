64edfe06624da7084820d0816e1a7b3ffdad30af
RianYuri <rianyurirgt@gmail.com>
2023-06-04 16:29:00 -0300

    feat: fix especialistas don t center

--- js/home/apiMaps.js
+++ js/home/apiMaps.js
@@ -1,47 +1,57 @@
 function initMap() {
-    const notGeolocation = document.querySelector(".not-geolocation")
+    const notGeolocation = document.querySelector(".not-geolocation");
     if (navigator.geolocation) {
-        notGeolocation.style.display = "none";
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
+      notGeolocation.style.display = "none";
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
           var userLocation = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           };
   
-          var map = new google.maps.Map(document.getElementById("map"), {
-            center: userLocation,
-            zoom: 16
-          });
+          // Use a API Geocoding para obter a cidade correspondente às coordenadas de geolocalização
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({ location: userLocation }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+              if (results[0]) {
+                // Obtenha a cidade da primeira resposta do Geocoding
+                var city = getCityFromGeocodingResults(results);
   
-          var request = {
-            location: userLocation,
-            radius: "30000",
-            type: "veterinary_care",
-            keyword: "veterinário"
-          };
+                var map = new google.maps.Map(document.getElementById("map"), {
+                  center: userLocation,
+                  zoom: 18
+                });
   
-          var service = new google.maps.places.PlacesService(map);
-          service.nearbySearch(request, function (results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-              var bounds = new google.maps.LatLngBounds();
-              for (var i = 0; i < results.length; i++) {
-                createMarker(results[i], map, bounds);
+                var request = {
+                  location: userLocation,
+                  radius: "15000",
+                  type: "veterinary_care",
+                  keyword: "veterinário",
+                  query: city // Use a cidade como parte da consulta para encontrar veterinários próximos à cidade do usuário
+                };
+  
+                var service = new google.maps.places.PlacesService(map);
+                service.textSearch(request, function (results, status) {
+                  if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    var bounds = new google.maps.LatLngBounds();
+                    for (var i = 0; i < results.length; i++) {
+                      createMarker(results[i], map, bounds);
+                    }
+                    map.fitBounds(bounds);
+                    map.setZoom(14); // Defina o nível de zoom desejado
+                  }
+                });
               }
-              map.fitBounds(bounds);
-              map.setZoom(14);
             }
           });
         },
         function (error) {
           console.log("Erro ao obter a localização do usuário: " + error.message);
-        notGeolocation.style.display = "flex";
-
+          notGeolocation.style.display = "flex";
         }
       );
     } else {
-        notGeolocation.style.display = "flex";
-        
+      notGeolocation.style.display = "flex";
       console.log("Geolocalização não é suportada pelo seu navegador.");
     }
   }
@@ -54,18 +64,37 @@ function initMap() {
     });
   
     var infowindow = new google.maps.InfoWindow({
-      content: '<div><strong>' + place.name + '</strong><br>' +
-        'Endereço: ' + place.vicinity + '<br>' +
-        '<a href="https://www.google.com/maps/dir/?api=1&destination=' + encodeURIComponent(place.vicinity) + '" target="_blank">Obter direções</a></div>'
+      content:
+        '<div><strong>' +
+        place.name +
+        '</strong><br>' +
+        'Endereço: ' +
+        place.formatted_address +
+        '<br>' +
+        '<a href="https://www.google.com/maps/dir/?api=1&destination=' +
+        encodeURIComponent(place.formatted_address) +
+        '" target="_blank">Obter direções</a></div>'
     });
   
     marker.addListener("click", function () {
-      infowindow.open(map, marker); 
-      map.setCenter(marker.getPosition()); 
-      map.setZoom(18); 
+      infowindow.open(map, marker);
+      map.setCenter(marker.getPosition());
+      map.setZoom(18);
     });
   
     bounds.extend(place.geometry.location);
+  }
+  
+  function getCityFromGeocodingResults(results) {
+    for (var i = 0; i < results.length; i++) {
+      for (var j = 0; j < results[i].address_components.length; j++) {
+        var component = results[i].address_components[j];
+        if (component.types.includes("locality")) {
+          return component.long_name;
+        }
+      }
+    }
+    return null;
   }
   
   window.initMap = initMap;
