d2a3adc9c11c85161fa5063e4c523a849743777a
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-06-11 18:49:04 -0300

    feat: first access tutorial

--- frontend/js/login/firstAccess.js
+++ frontend/js/login/firstAccess.js
@@ -1,6 +1,26 @@
 
-console.log('firstAccess.js loaded', JSON.parse(localStorage.getItem('isNewUser')))
 if(JSON.parse(localStorage.getItem('isNewUser'))){
-    document.querySelector('.home-container').classList.toggle('hidden')
-    document.querySelector('.first__access-page').classList.toggle('hidden')
+    document.querySelector('.home-container').classList.add('hidden')
+    document.querySelector('.first__access-page').classList.remove('hidden')
+}
+const firstAccessNextSlider = () => {
+    let slider = document.querySelector('.first__access-slider')
+    let sliderItemWidth = document.querySelector('.first__access-slider .first__access-texts').clientWidth
+    let counterInactive = document.querySelector('.first__access-count:not(.active)')
+    if(counterInactive){
+        slider.scrollLeft += sliderItemWidth
+        let counterAfterActive = document.querySelector('.first__access-count.active:not(.actived__before)')
+        counterAfterActive.classList.add('actived__before')
+        counterInactive.classList.add('active')
+    }else{
+        localStorage.setItem('isNewUser', JSON.stringify(false))
+        document.querySelector('.first__access-page').classList.add('hidden')
+        document.querySelector('.home-container').classList.remove('hidden')
+    }
+}
+
+const firstAccessSkipSlider = () => {
+    localStorage.setItem('isNewUser', JSON.stringify(false))
+    document.querySelector('.first__access-page').classList.add('hidden')
+    document.querySelector('.home-container').classList.remove('hidden')
 }
